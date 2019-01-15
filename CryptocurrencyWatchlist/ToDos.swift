c064dc2e60ada2fb571bb1c40e0a7897334579ef
Yunus Yurttagul <YunusYurttagul@Yunus-MBP.local>
2019-01-15 01:01:02 -0800

    ...

---
+++ CryptocurrencyWatchlist/ToDos.swift
@@ -0,0 +1,11 @@
+//
+//  ToDos.swift
+//  CryptocurrencyWatchlist
+//
+//  Created by Yunus Yurttagul on 15.01.2019.
+//  Copyright © 2019 YJ. All rights reserved.
+//
+
+// Cryptos should always be in order, when new one is added, order is changed
+// Auto refresh for prices
+
